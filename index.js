c03008c8a1dee3b44b5a00a1ec7656041edff640
Jerry Tsai <jerry@owl.co>
2022-08-02 21:20:17 -0700

    fix: target loss after submit

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 const DICTIONARY = {
-  xi3: "喜",
-  huan1: "歡",
-  bi3: "比",
-  jiao4: "較",
-  shi4: "適",
-  he2: "合",
-  mian4: "麵",
-  fan4: "飯",
-  wei4: "味",
-  ping2: "平",
-  chang2: "常",
-  zhe4: "這",
-  yang4: "樣",
-  hen3: "很",
-  qu3: "取",
-  na2: "拿",
+  xi3: '喜',
+  huan1: '歡',
+  bi3: '比',
+  jiao4: '較',
+  shi4: '適',
+  he2: '合',
+  mian4: '麵',
+  fan4: '飯',
+  wei4: '味',
+  ping2: '平',
+  chang2: '常',
+  zhe4: '這',
+  yang4: '樣',
+  hen3: '很',
+  qu3: '取',
+  na2: '拿',
 };
 
 const WORDS = Object.values(DICTIONARY);
 
-const character = document.getElementById("character");
-const input = document.getElementById("input");
-const form = document.getElementById("form");
+const character = document.getElementById('character');
+const input = document.getElementById('input');
+const form = document.getElementById('form');
 
 const fetchRandomIndex = () => Math.floor(Math.random() * (WORDS.length));
 const submitAnswer = (e) => {
   e.preventDefault();
 
-  if (DICTIONARY[e.target[0].value] === character.innerText) {
+  if (DICTIONARY[e.currentTarget[0].value] === character.innerText) {
     character.innerText = WORDS[fetchRandomIndex()];
+    e.currentTarget[0].value = '';
   }
-
-  input.value = "";
 };
 
-form.addEventListener("submit", submitAnswer);
+form.addEventListener('submit', submitAnswer);
+
 character.innerText = WORDS[fetchRandomIndex()];
