ff773d87708ae30ec8d5541aae0043828a4bd4a5
Jerry Tsai <jerry@owl.co>
2023-04-27 17:52:28 -0700

    feat: support multiple characters per pinyin

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 const DICTIONARY = {
-  xi3: '喜',
-  huan1: '歡',
-  bi3: '比',
-  jiao4: '較',
-  shi4: '適',
-  he2: '合',
-  mian4: '麵',
-  fan4: '飯',
-  wei4: '味',
-  ping2: '平',
-  chang2: '常',
-  zhe4: '這',
-  yang4: '樣',
-  hen3: '很',
-  qu3: '取',
-  na2: '拿',  
-  zhou1: '週',
-  pei2: '陪',
-  peng2: '朋',
-  guang4: '逛',
-  jie1: '街',
-  ta: '她',
-  zhu1: '珠',
-  bao3: '寶',
-  chu1: '出',
-  qu4: '去',
-  sai4: '賽',
-  chang3: '場',
-  li3: '禮',
-  bai4: '拜',
+  bai4: ['拜'],
+  bao3: ['寶'],
+  bi3: ['比'],
+  chang2: ['常'],
+  chang3: ['場'],
+  chu1: ['出'],
+  fan4: ['飯'],
+  guang4: ['逛'],
+  he2: ['合'],
+  hen3: ['很'],
+  huan1: ['歡'],
+  jiao4: ['較'],
+  jie1: ['街'],
+  li3: ['禮'],
+  mian4: ['麵'],
+  na2: ['拿'],  
+  pei2: ['陪'],
+  peng2: ['朋'],
+  ping2: ['平'],
+  qu3: ['取'],
+  qu4: ['去'],
+  sai4: ['賽'],
+  shi4: ['適'],
+  ta: ['她'],
+  xi3: ['喜'],
+  wei4: ['味'],
+  yang4: ['樣'],
+  zhe4: ['這'],
+  zhou1: ['週'],
+  zhu1: ['珠'],
 };
 
 let WORDS = Object.values(DICTIONARY);
@@ -37,14 +37,22 @@ const character = document.getElementById('character');
 const input = document.getElementById('input');
 const form = document.getElementById('form');
 
-const fetchRandomIndex = () => Math.floor(Math.random() * (WORDS.length));
+const fetchRandomIndex = (dictionary) => Math.floor(Math.random() * (dictionary.length));
+
 const submitAnswer = (e) => {
   e.preventDefault();
 
-  if (DICTIONARY[e.currentTarget[0].value] === character.innerText) {
+  if (DICTIONARY[e.currentTarget[0].value].indexOf(character.innerText) > -1) {
     delete DICTIONARY[e.currentTarget[0].value];
     WORDS = Object.values(DICTIONARY);
-    character.innerText = WORDS.length > 0 ? WORDS[fetchRandomIndex()] : 'Finished!';
+
+    if (WORDS.length > 0) {
+      const pinyinIndex = fetchRandomIndex(WORDS);
+      const characterIndex = fetchRandomIndex(WORDS[pinyinIndex]);
+      character.innerText = WORDS[pinyinIndex][characterIndex];
+    } else {
+      character.innerText = 'Finished!';  
+    }
   }
 
   e.currentTarget[0].value = '';
@@ -52,4 +60,6 @@ const submitAnswer = (e) => {
 
 form.addEventListener('submit', submitAnswer);
 
-character.innerText = WORDS[fetchRandomIndex()];
+const pinyinIndex = fetchRandomIndex(WORDS);
+const characterIndex = fetchRandomIndex(WORDS[pinyinIndex]);
+character.innerText = WORDS[pinyinIndex][characterIndex];
